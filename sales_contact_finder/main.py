7182be86a87ed99730c10dc1a9b89df239b773f0
Vivek Sejpal <vsejpal@gmail.com>
2025-04-11 19:01:45 -0700

    remove unsed commands train, replay, test

--- sales_contact_finder/main.py
+++ sales_contact_finder/main.py
@@ -22,45 +22,6 @@ def run():
     print(result)
 
 
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {"topic": "AI LLMs"}
-    try:
-        SalesContactFinderCrew().crew().train(
-            n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs
-        )
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        SalesContactFinderCrew().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {"topic": "AI LLMs"}
-    try:
-        SalesContactFinderCrew().crew().test(
-            n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs
-        )
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please specify a command: run")
